6e826cd3b0886d8728bb39ed2b06fbc214a3d8f4
andre luiz martins santiago <santiiagoo2405@gmail.com>
2026-03-31 17:25:46 -0300

    tentativa

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require(`express`)
 
  const app = express()
 
+ const users = []
+
+ app.post(`/user`, (req, res) => {
+   
+   console.log(req)
+   
+   res.send(`ok`)
+ })
+
  app.get(`/user`, (req, res) => {
     res.send(`ok,deu bom`)
  })
